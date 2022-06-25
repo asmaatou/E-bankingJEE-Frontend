83350503478f99ad036bf15319c9cbf3bb1c48c1
Tounsi Asmaa <tasmaa2909@gmail.com>
2022-06-25 17:14:14 +0100

    3rd commit

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {CustomerService} from "../services/customer.service";
+import {catchError, Observable, throwError} from "rxjs";
+import {Customer} from "../model/customer.model";
 
 @Component({
   selector: 'app-customers',
@@ -7,15 +10,18 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit {
-  customers : any;
-  constructor(private http:HttpClient) { }
+  customers! : Observable<Array<Customer>>;
+  errorMessage! : string;
+
+  constructor(private customerService: CustomerService) { }
 
   ngOnInit(): void {
-    this.http.get("http://localhost:8086/customers").subscribe(data=> {
-      this.customers = data;
-    },error => {
-      console.log(error);
-    })
+    this.customers=this.customerService.getCustomers().pipe(
+      catchError(err=>{
+        this.errorMessage=err.message;
+        return throwError(err);
+      })
+    );
   }
 
 }
